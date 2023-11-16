1363d8bf127dca16296c99956d9e2374d3901974
sairamyagajula <ramyaroyal1639@gmail.com>
2023-11-15 18:53:14 -0600

    updated vehicles.js in routes

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -8,4 +8,5 @@ router.get('/', vehicles_controller.vehicles_view_all_Page );
 module.exports = router;
 
 /* GET detail vehicles page */
-router.get('/detail', vehicles_controlers.vehicles_view_one_Page);
+router.get('/detail', vehicles_controller.vehicles_view_one_Page);
+
