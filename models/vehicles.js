781b1047512e3b8b056f3e0c202325d5fb78ebe4
sairamyagajula <ramyaroyal1639@gmail.com>
2023-11-29 19:43:01 -0600

    Updated vehicles.js

--- models/vehicles.js
+++ models/vehicles.js
@@ -1,7 +1,35 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
-    name :String,
-    mileage : Number,
-    color : String
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        // Custom validator using RegExp
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9_]+$/.test(v); // Ensure length of vehicle between 3 and 30
+            },
+            message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+        },
+    },
+    mileage: {
+        type: Number,
+        validate: {
+            validator: function(v) {
+                return v >= 0; 
+            },
+            message: props => `${props.value} is not a valid mileage! Mileage must be a non-negative number.`,
+        },
+    },
+    color: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z0-9_]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid color! Use only letters, numbers, and underscores.`,
+        },
+    },
 })
 module.exports = mongoose.model("vehicles", vehicleSchema)
